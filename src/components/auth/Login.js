473e31a4d05187f0ffc0e6af5d1b5a37e5cfde5c
Sagar Adval <sagaradval@gmail.com>
2021-08-25 01:28:03 +0530

    Created Login component(JSX)

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,7 +1,31 @@
 import React from "react";
+import LoginImage from "../../assets/images/login.svg";
 
 const Login = () => {
-  return <h1>Login page</h1>;
+  return (
+    <div id="auth-container">
+      <div id="auth-card">
+        <div>
+          <div id="image-section">
+            <img src={LoginImage}></img>
+          </div>
+          <div id="form-section">
+            <h2>Welcome Back!</h2>
+            <form>
+              <div className="input-field">
+                <input placeholder="Email"></input>
+              </div>
+              <div className="input-field">
+                <input placeholder="Password"></input>
+              </div>
+              <button>LOGIN</button>
+            </form>
+            <p>Don't have an account? Register now!</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
