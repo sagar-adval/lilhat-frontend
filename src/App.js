9d88f4c2961b608cdb38773f9f6ab9f4fd8cfc30
Sagar Adval <sagaradval@gmail.com>
2021-08-25 01:19:21 +0530

    Components and router setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import "./App.css";
+import Login from "./components/auth/Login";
+import Register from "./components/auth/Register";
+import Chat from "./components/chat/chat";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Lilhat</h1>
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route exact path="/" component={Chat} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/register" component={Register} />
+          <Route render={() => <h1>Looks like you're lost</h1>} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
