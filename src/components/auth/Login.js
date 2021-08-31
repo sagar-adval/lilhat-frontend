7532ee2bb4967ff62e0893faa87d9684a423052d
Sagar Adval <sagaradval@gmail.com>
2021-08-31 18:46:06 +0530

    Login page styling done

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,10 +1,11 @@
 import React from "react";
+import "./auth.scss";
 import LoginImage from "../../assets/images/login.svg";
 
 const Login = () => {
   return (
     <div id="auth-container">
-      <div id="auth-card">
+      <div id="auth-card" className="card-shadow">
         <div>
           <div id="image-section">
             <img src={LoginImage}></img>
@@ -12,10 +13,10 @@ const Login = () => {
           <div id="form-section">
             <h2>Welcome Back!</h2>
             <form>
-              <div className="input-field">
+              <div className="input-field mb-1">
                 <input placeholder="Email"></input>
               </div>
-              <div className="input-field">
+              <div className="input-field mb-2">
                 <input placeholder="Password"></input>
               </div>
               <button>LOGIN</button>
